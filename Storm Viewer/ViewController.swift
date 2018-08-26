55eb180d0d2d7cfc900415296b68ee268be90dde
Edvin Lellhame <edvinlel@gmail.com>
2018-08-26 13:27:48 -0700

    Initial Commit

---
+++ Storm Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storm Viewer
+//
+//  Created by Edvin Lellhame on 8/26/18.
+//  Copyright © 2018 Edvin Lellhame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
